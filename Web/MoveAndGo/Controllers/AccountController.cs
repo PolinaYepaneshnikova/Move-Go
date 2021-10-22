c6a2e9d3881e4bffc1ab3e3fc94d562cb6a16eb6
SergeyCherevan <CherevanSergeyLugansk@gmail.com>
2021-10-22 12:31:01 +0300

    Add Account controller

---
+++ Web/MoveAndGo/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using MoveAndGo.Models;
+
+namespace MoveAndGo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<User> _manager;
+        private readonly SignInManager<User> _signInManager;
+        public AccountController(UserManager<User> userMgr, SignInManager<User> signinMgr)
+        {
+            _manager = userMgr;
+            _signInManager = signinMgr;
+        }
+    }
+}
