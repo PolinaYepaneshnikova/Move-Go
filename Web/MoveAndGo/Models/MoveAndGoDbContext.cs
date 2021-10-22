7e7e0dff5ebcafde98d05d5a978aac4dddaa53d5
SergeyCherevan <CherevanSergeyLugansk@gmail.com>
2021-10-22 11:16:33 +0300

    Add: database classes User & DatabaseContext

---
+++ Web/MoveAndGo/Models/MoveAndGoDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace MoveAndGo.Models
+{
+    public class MoveAndGoContext : IdentityDbContext<User>
+    {
+        public MoveAndGoContext(DbContextOptions<MoveAndGoContext> options) : base(options) { }
+    }
+}
---
+++ Web/MoveAndGo/Models/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MoveAndGo.Models
+{
+    public class User : IdentityUser
+    {
+        public string Avatar { get; set; }
+        public string Biographi { get; set; }
+    }
+}
