99045d755327a970fe73bb14bbc030e54af41cb1
Andrii Yahniukov <andrii.yahniukov@gmail.com>
2021-10-28 22:44:41 +0300

    Refactoring code for React

--- Web/MoveAndGo/ClientApp/src/index.js
+++ Web/MoveAndGo/ClientApp/src/index.js
@@ -2,15 +2,13 @@
 import './style.scss';
 import 'bootstrap/dist/css/bootstrap.css';
 //React
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 //Component
 import App from './components/app/app';
 
 ReactDOM.render(
-  <BrowserRouter basename={document.getElementsByTagName('base')[0].getAttribute('href')}>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('wrapper'));
+  <App />,
+  document.getElementById('wrapper')
+);
 
