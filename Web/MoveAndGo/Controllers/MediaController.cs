fb6c270b010de82defd0e87903f82cd0eac0e299
SergeyCherevan <CherevanSergeyLugansk@gmail.com>
2021-10-29 19:01:03 +0300

    Add: Media controller. Don't work

---
+++ Web/MoveAndGo/Controllers/MediaController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MoveAndGo.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MediaController : Controller
+    {
+        private readonly IWebHostEnvironment _appEnvironment;
+
+        public MediaController(IWebHostEnvironment appEnviroment)
+        {
+            _appEnvironment = appEnviroment;
+        }
+
+        public IActionResult Avatar(string id)
+        {
+            System.Console.WriteLine(_appEnvironment.ContentRootPath + "/ResourceFiles/Avatars/" + id);
+
+            return new PhysicalFileResult(_appEnvironment.ContentRootPath + "/ResourceFiles/Avatars/" + id, "image/jpeg");
+        }
+
+        public IActionResult Video(string id)
+        {
+            System.Console.WriteLine(_appEnvironment.ContentRootPath + "/ResourceFiles/Video/" + id);
+
+            return new PhysicalFileResult(_appEnvironment.ContentRootPath + "/ResourceFiles/Video/" + id, "video/jpeg");
+        }
+    }
+}
