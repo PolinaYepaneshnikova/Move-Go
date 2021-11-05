a77b497f618fcabd8e09bbb4373253f509d3092f
Andrii Yahniukov <andrii.yahniukov@gmail.com>
2021-11-05 14:32:09 +0200

    Add: nav menu

--- Web/MoveAndGo/ClientApp/src/components/appUpHeader/appUpHeader.js
+++ Web/MoveAndGo/ClientApp/src/components/appUpHeader/appUpHeader.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import './appUpHeader.scss';
 import avatar from '../../img/profile-no-photo.png';
 
@@ -7,10 +7,12 @@ export default function AppUpHeader() {
   return (
     <header className="appUpHeader">
       <Link to='/' className='appUpHeader-title'>{"Move&GO"}</Link>
-      <div className='appUpHeader__block'>
-        <input type='text' className='appUpHeader__block-input' placeholder='Search' />
-        <Link to='/' className='appUpHeader__block-link'><img src={avatar} alt="photo_user" className='appUpHeader__block-photo' /></Link>
-      </div>
+      <input type='text' className='appUpHeader-input' placeholder='Search' />
+      <ul className='appUpHeader__menu'>
+        <NavLink to='/' className='appUpHeader__menu-link'><i className="fas fa-home appUpHeader__menu-link"></i></NavLink>
+        <NavLink to='/add' className='appUpHeader__menu-link'><i className="fas fa-plus-square appUpHeader__menu-link"></i></NavLink>
+        <NavLink to='/profile' className='appUpHeader__menu-link'><img src={avatar} alt="photo_user" className='appUpHeader__menu-photo' /></NavLink>
+      </ul>
     </header>
   );
 }
