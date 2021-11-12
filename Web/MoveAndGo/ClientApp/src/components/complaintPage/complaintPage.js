bd25f376be58b7c2395da47f497dd712dfc07bea
Andrii Yahniukov <andrii.iahniukov@nure.ua>
2021-11-12 11:59:48 +0200

    Add: req to server complaints

--- Web/MoveAndGo/ClientApp/src/components/complaintPage/complaintPage.js
+++ Web/MoveAndGo/ClientApp/src/components/complaintPage/complaintPage.js
@@ -1,28 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './complaintPage.scss';
+import getRequest from '../../services/getRequest';
 
 export default function ComplaintPage() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    complaintData();
+  }, []);
+
+  const onSuccess = (data) => {
+    setData(data);
+  }
+
+  const complaintData = () => {
+    getRequest("/api/admin/getadminnotifications")
+      .then(onSuccess);
+  }
+
   return (
     <div className='complaint'>
       <ul className='complaint__list'>
-        <li className='complaint__list-item'>
-          <span className='complaint__list-item__text'><b>URL</b></span>
-          <a href='/complaints' className='complaint__list-item__link'>https://localhost:44346/complaints</a>
-          <span className='complaint__list-item__text'><b>Text</b></span>
-          <span className='complaint__list-item__text'>disgusting content</span>
-        </li>
-        <li className='complaint__list-item'>
-          <span className='complaint__list-item__text'><b>URL</b></span>
-          <a href='/complaints' className='complaint__list-item__link'>https://localhost:44346/complaints</a>
-          <span className='complaint__list-item__text'><b>Text</b></span>
-          <span className='complaint__list-item__text'>disgusting content</span>
-        </li>
-        <li className='complaint__list-item'>
-          <span className='complaint__list-item__text'><b>URL</b></span>
-          <a href='/complaints' className='complaint__list-item__link'>https://localhost:44346/complaints</a>
-          <span className='complaint__list-item__text'><b>Text</b></span>
-          <span className='complaint__list-item__text'>disgusting content</span>
-        </li>
+        {
+          data.map(item => {
+            return (
+              <li className='complaint__list-item' key={item?.id}>
+                <span className='complaint__list-item__text'><b>URL</b></span>
+                <a href={`/api${item?.itemLink}`} className='complaint__list-item__link'>{`https://localhost:5001/api${item?.itemLink}`}</a>
+                <span className='complaint__list-item__text'><b>Text</b></span>
+                <span className='complaint__list-item__text'>{item?.text ?? ""}</span>
+              </li>
+            )
+          })
+        }
       </ul>
     </div>
   );
