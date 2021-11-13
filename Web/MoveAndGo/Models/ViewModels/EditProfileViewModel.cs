ee54ff006c07e3d5ee657c8b40fff0ea6c8f1482
SergeyCherevan <CherevanSergeyLugansk@gmail.com>
2021-11-13 17:10:09 +0200

    Add: EditProfile action to Account controller

---
+++ Web/MoveAndGo/Models/ViewModels/EditProfileViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace MoveAndGo.Models.ViewModels
+{
+    public class EditProfileViewModel
+    {
+        [UIHint("Avatar")]
+        [Display(Name = "Avatar")]
+        public IFormFile Avatar { get; set; }
+
+        [UIHint("Full name")]
+        [Display(Name = "Full name")]
+        public string FullName { get; set; }
+
+        [UIHint("Bio")]
+        [Display(Name = "Bio")]
+        public string Bio { get; set; }
+
+        [UIHint("Email")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [UIHint("Phone")]
+        [Display(Name = "Phone")]
+        public string Phone { get; set; }
+
+        [UIHint("OldPassword")]
+        [Display(Name = "OldPassword")]
+        public string OldPassword { get; set; }
+
+        [UIHint("NewPassword")]
+        [Display(Name = "NewPassword")]
+        public string NewPassword { get; set; }
+
+        [Display(Name = "Confirm the entered password")]
+        [UIHint("NewPassword")]
+        [Compare("NewPassword", ErrorMessage = "Password mismatch")]
+        public string ConfirmNewPassword { get; set; }
+    }
+}
