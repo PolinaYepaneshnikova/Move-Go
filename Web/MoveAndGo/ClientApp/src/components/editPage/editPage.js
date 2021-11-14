fd63078b9d82a0ca540d63498545d70964532431
Andrii Yahniukov <andrii.iahniukov@nure.ua>
2021-11-14 14:59:08 +0200

    Add: connect to server for edit info

--- Web/MoveAndGo/ClientApp/src/components/editPage/editPage.js
+++ Web/MoveAndGo/ClientApp/src/components/editPage/editPage.js
@@ -15,57 +15,42 @@ export default function EditPage() {
     getRequest('/api/account/currentuser')
       .then(onSuccess);
   }
+
   return (
     <div className="editForm-body">
-      <form className="editForm">
+      <form className="editForm" encType="multipart/form-data" accesskeymethod="Post" action="/api/account/editprofile" method="post">
         <h1 className="editForm-title">Edit profile</h1>
         <div className="editForm-block">
           <span className="editForm-label">Avatar</span>
           <input
-            minLength="4"
-            maxLength="32"
             className="editForm-input editForm-avatar"
             type="file"
             name="avatar"
             tabIndex="1"
-            required
+            defaultValue={data?.avatar}
           />
         </div>
         <div className="editForm-block">
-          <span className="editForm-label">Name</span>
+          <span className="editForm-label">Full Name</span>
           <input
             minLength="4"
             maxLength="32"
             className="editForm-input"
             type="text"
-            value={data?.fullName ?? ""}
+            defaultValue={data?.fullName}
             name="fullname"
-            tabIndex="1"
-            required
-          />
-        </div>
-        <div className="editForm-block">
-          <span className="editForm-label">Nickname</span>
-          <input
-            className="editForm-input"
-            type="text"
-            name="nickname"
             tabIndex="2"
-            value={data?.userName}
-            required
           />
         </div>
         <div className="editForm-block">
           <span className="editForm-label">Bio</span>
           <textarea
             minLength="4"
-            maxLength="32"
+            maxLength="200"
             className="editForm-input editForm-textarea"
-            type="text"
             name="bio"
-            value={data?.biographi ?? ""}
+            defaultValue={data?.biographi ?? ""}
             tabIndex="3"
-            required
           ></textarea>
         </div>
         <div className="editForm-block">
@@ -76,25 +61,56 @@ export default function EditPage() {
             className="editForm-input"
             type="email"
             name="email"
-            value={data?.email ?? ""}
+            defaultValue={data?.email}
             tabIndex="4"
-            required
           />
         </div>
         <div className="editForm-block">
           <span className="editForm-label">Phone number</span>
           <input
             minLength="4"
-            maxLength="200"
+            maxLength="32"
             className="editForm-input"
             type="tel"
             name="phonenumber"
-            value={data?.phoneNumber ?? ""}
+            defaultValue={data?.phoneNumber ?? ""}
             tabIndex="5"
-            required
           />
         </div>
-        <button className="editForm-btn" tabIndex="6">Submit</button>
+        <div className="editForm-block">
+          <span className="editForm-label">New password</span>
+          <input
+            minLength="4"
+            maxLength="32"
+            className="editForm-input"
+            type="password"
+            name="newpassword"
+            tabIndex="6"
+          />
+        </div>
+        <div className="editForm-block">
+          <span className="editForm-label">Confirm new password</span>
+          <input
+            minLength="4"
+            maxLength="32"
+            className="editForm-input"
+            type="password"
+            name="confirmnewpassword"
+            tabIndex="7"
+          />
+        </div>
+        <div className="editForm-block">
+          <span className="editForm-label">Old password</span>
+          <input
+            minLength="4"
+            maxLength="32"
+            className="editForm-input"
+            type="password"
+            name="oldpassword"
+            tabIndex="8"
+          />
+        </div>
+        <button className="editForm-btn" tabIndex="9">Submit</button>
       </form>
     </div>
   );
